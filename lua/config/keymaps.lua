e21b300953eecb02bede7fc21e53c3daefe0fdbb
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-10-17 07:38:58 +0700

    switched root keymap to file browser telescope extnesion

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,30 @@
-
 vim.api.nvim_set_keymap("n", "yy", "1GvG$y", { noremap = true, silent = true })
 
 -- Telescope keymaps
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Space>e", ":Telescope file_browser<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Space>e", "", {
+	noremap = true,
+	silent = true,
+	callback = function()
+		local telescope = require("telescope")
+
+		telescope.extensions.file_browser.file_browser({
+			path = "/",
+			cwd = "/",
+			respect_gitignore = false,
+			hidden = true,
+			grouped = true,
+			previewer = true,
+			initial_mode = "normal",
+			layout_strategy = "horizontal",
+			layout_config = {
+				height = 0.8,
+				width = 0.95,
+				preview_width = 0.6,
+			},
+		})
+	end,
+})
 
 -- Buffer keymaps
 vim.api.nvim_set_keymap("n", "BB", ":BufferClose<CR>", { noremap = true, silent = true })
